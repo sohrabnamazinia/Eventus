c22f3c5274f7f33875e34a480bbe0a579a9f7ad5
unknown <sohrabsn7@yahoo.com>
2020-10-17 18:00:33 +0330

    init

--- ArsamBackend/ArsamBackend/Startup.cs
+++ ArsamBackend/ArsamBackend/Startup.cs
@@ -31,6 +31,7 @@ namespace ArsamBackend
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
