d50dafe1811b5b3db155b1a2f6da7f4eeb96ac6e
ali <ali.farahat78@gmail.com>
2020-10-26 18:11:12 +0330

    viewmodels for events

---
+++ ArsamBackend/ArsamBackend/ViewModels/Event.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ArsamBackend.ViewModels
+{
+    public class EventViewModel
+    {
+        [Required]
+        public string Name { get; set; }
+
+        public bool IsPrivate { get; set; }
+
+        public string PasswordConfirmation { get; set; }
+
+        public string Location { get; set; }
+
+
+    }
+}
